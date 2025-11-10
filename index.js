d41c8421b4aed0cded6ba4335fb303b8cad761b5
barongobruce <brosnanbarongo@gmail.com>
2025-11-10 19:51:10 +0300

    Completed arrow functions and passing tests

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1. Function Expression for 'divide'
+// Test requirement: has a function expression called divide
+const divide = function(a, b) {
+  return a / b;
+};
+
+// 2. Arrow Function for 'square'
+// Test requirement: has an arrow function called square
+const square = (n) => {
+  return n * n;
+};
+
+// 3. Arrow Function for 'add'
+// Test requirement: has an arrow function called add
+const add = (a, b) => {
+  return a + b;
+};
